5ac503f7579843ef20520fbd3adfe10c04fb2897
DESKTOP-CQQSGQK\parry <peixingborx@163.com>
2025-04-18 12:33:05 +0800

    fix: import path error

--- netlify/edge-functions/menu.js
+++ netlify/edge-functions/menu.js
@@ -1,8 +1,8 @@
-import { PrismaClient } from '../generated/prisma'
+import { PrismaClient } from '../../generated/prisma'
 
 const prisma = new PrismaClient()
 
 
-export default async () => new Response(JSON.stringify(await prisma.menu.findMany()));
+export default async () => new Response(await prisma.menu.findMany());
 
 export const config = { path: "/menus", method: 'GET' };
--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,18 @@ const prisma = new PrismaClient()
 
 async function main() {
   // Create a new post
-  const post = await prisma.post.create({
-    data: {
-      title: 'Hello World',
-      content: 'This is my first post!',
-      published: true,
-    },
-  })
-  console.log('Created post:', post)
+  // const post = await prisma.post.create({
+  //   data: {
+  //     title: 'Hello World',
+  //     content: 'This is my first post!',
+  //     published: true,
+  //   },
+  // })
+  // console.log('Created post:', post)
 
   // Get all posts
   const posts = await prisma.post.findMany()
-  console.log('All posts:', posts)
+  console.log('All posts:', typeof posts)
 }
 
 main()
