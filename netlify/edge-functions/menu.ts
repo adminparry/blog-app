6e82ccda7d58ca3f02a10b5330aa75e79f6c1981
DESKTOP-CQQSGQK\parry <peixingborx@163.com>
2025-04-18 12:39:42 +0800

    fix: ts

--- netlify/edge-functions/menu.ts
+++ netlify/edge-functions/menu.ts
@@ -3,6 +3,6 @@ import { PrismaClient } from '../../generated/prisma'
 const prisma = new PrismaClient()
 
 
-export default async () => new Response(await prisma.menu.findMany());
+export default async () => new Response(JSON.stringify(await prisma.menu.findMany()));
 
 export const config = { path: "/menus", method: 'GET' };
