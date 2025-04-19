0df425dc1a0c74869238dd0f886f632b894d6c43
汪超 <>
2025-04-19 11:31:51 +0800

    sd;:

--- edge-functions/hello.ts
+++ edge-functions/hello.ts
@@ -1,5 +1,4 @@
 import type { Config } from "@netlify/edge-functions"
-import { name } from '../constant'
 export default () => new Response('name');
 
 export const config:Config = { path: "/name" };
